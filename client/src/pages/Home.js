f4c50bccd39e0031bb259544f5d6cb3014ac87aa
[Shaikh Faruk] <[sfaruk1137@gmail.com]>
2021-09-23 23:04:56 +0530

    init

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>
-      <h1>home</h1>
-    </div>
+    <>
+      <div className="navbar">
+        <div className="logo-side">
+          <h1>QuantaVid</h1>
+        </div>
+        <div className="button-side">
+          <Link to="/login" type="button" className="toggle-btn">
+            Log In
+          </Link>
+          <Link to="/register" type="button" className="toggle-btn">
+            Register
+          </Link>
+        </div>
+      </div>
+    </>
   );
 };
 
