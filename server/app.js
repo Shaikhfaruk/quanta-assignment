fc083b291d53b9d09ee806fcc824962b376efe23
[Shaikh Faruk] <[sfaruk1137@gmail.com]>
2021-09-25 17:20:34 +0530

    make it better

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 require("dotenv").config();
 const cors = require("cors");
+const path = require("path");
 
 //connect our database
 mongoose
@@ -20,6 +21,15 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/auth", require("./routes/user"));
+
+// Server build
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
