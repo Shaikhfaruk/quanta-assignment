8f6cbcbdd9da10348214f97a13fa69f1e2e25a9f
[Shaikh Faruk] <[sfaruk1137@gmail.com]>
2021-09-25 18:00:04 +0530

    fix problem

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
+const cors = require("cors");
+const path = require("path");
+
+//connect our database
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("database connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+app.use(cors());
+app.use(express.json());
+
+app.use("/auth", require("./client/routes/user"));
+
+// Server build
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server is running at ${PORT}`));
